daeeeb99055f38e27c2afd3c7ed8d5faa87dce8e
sshhsh <sshhsh@foxmail.com>
2018-05-29 09:48:08 +0800

    worked

--- duanwenxue.py
+++ duanwenxue.py
@@ -5,24 +5,28 @@ import re
 import time
 from bs4 import BeautifulSoup
 
-for i in range(4729413, 1, -1):
-    url = "https://www.duanwenxue.com/article/%d.html" % i
+
+def get_article(url):
+    time.sleep(12)
     try:
         resp = urllib.request.urlopen(url)
     except HTTPError as e:
         print('The server could not fulfill the request.')
         print('Error code: ', e.code)
-        continue
+        return
     except URLError as e:
         print('We failed to reach a server.')
         print('Reason: ', e.reason)
-        continue
+        return
+    except Exception as e:
+        print("strange in %s", url)
+        return
     html = resp.read()
     bs = BeautifulSoup(html, "html.parser", from_encoding="gb18030")
     title = re.sub('<.*?>', '', str(bs.h1))
     print(title)
-    publishDate = re.search('[0-9]{4}-[0-9]{2}-[0-9]{2}', str(bs.find(class_="text")))
-    print(publishDate.group())
+    publish_date = re.search('[0-9]{4}-[0-9]{2}-[0-9]{2}', str(bs.find(class_="text")))
+    print(publish_date.group())
     content = str(bs.find(class_=re.compile('article-content.*')))
     content2 = re.search('</span>\n(<p>.*?</p>\n)*', content, re.S)
 
@@ -30,14 +34,14 @@ for i in range(4729413, 1, -1):
     print(content3)
     if len(content3) <= 10:
         print("failed to crawl the content")
-        continue
-    searchAuthor = re.search('<span>(.*)</span>', str(bs.find(class_="article-writer")))
-    print(searchAuthor.group(1))
+        return
+    search_author = re.search('<span>(.*)</span>', str(bs.find(class_="article-writer")))
+    print(search_author.group(1))
     doc = {
         "title": title,
         "content": content3,
-        "author": searchAuthor.group(1),
-        "time": publishDate.group(),
+        "author": search_author.group(1),
+        "time": publish_date.group(),
         "url": url
     }
     data = json.dumps(doc, ensure_ascii=False)
@@ -45,4 +49,27 @@ for i in range(4729413, 1, -1):
     with open('duanwenxue.txt', 'a') as f:
         f.write(data)
         f.write('\n')
-    time.sleep(0.6)
+
+
+address_list = [
+    '/shanggan/rizhi/',
+]
+
+
+def crawl(url):
+    try:
+        resp = urllib.request.urlopen(url)
+    except Exception as e:
+        print("failed in %s", url)
+        return
+    html = str(resp.read().decode('gb18030'))
+    search_article = re.findall('href="(/article/[0-9]*.html)',html)
+    for address in search_article:
+        get_article('https://www.duanwenxue.com' + address)
+    search_next = re.search(u'<a href="(.*?)">\u4e0b\u4e00\u9875</a>', html)
+    new_url = re.sub('list.*', '', url) + search_next.group(1)
+    crawl(new_url)
+
+
+for address in address_list:
+    crawl('https://www.duanwenxue.com' + address)
